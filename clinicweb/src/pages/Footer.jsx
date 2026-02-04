628ea61fbab4ce0615fbb837f4373ec54f29dd90
shailendra pratap <singhlalprat@gmail.com>
2026-02-04 13:10:05 +0530

    Update Footer.jsx

--- clinicweb/src/pages/Footer.jsx
+++ clinicweb/src/pages/Footer.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 
+const sections = [
+   
+   {
+    title:'Home',
+    links:['home']
+   } ,
+   {
+        title:' About us',
+        links:['about']
+    },
+    {
+        title:'Testimonials',
+        links:['testimonial']
+    },
+    {
+        title:'History',
+        links:['history']
+    }
+]
+
 const Footer = () => {
   return (
-    <section className='min-h-screen bg-white'>
+    <footer className='min-h-screen bg-white'>
    
-    </section>
+    </footer>
   )
 }
 
