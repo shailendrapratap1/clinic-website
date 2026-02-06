3b7352bcc6b20fbc673475c542112afd378ba602
shailendra pratap <singhlalprat@gmail.com>
2026-02-06 18:34:29 +0530

    routes added

--- clinicweb/src/App.jsx
+++ clinicweb/src/App.jsx
@@ -1,4 +1,5 @@
-
+import { useEffect } from 'react';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import './App.css'
 import './index.css'
 import Navbar from './pages/Navbar'
@@ -10,23 +11,52 @@ import Footer from './pages/Footer'
 import Signup from './pages/Signup'
 import Login from './pages/Login'
 
+ const ScrollToSection = () =>{
+  const location = useLocation();
+
+  useEffect(()=>{
+    const path = location.pathname;
+    if(path === '/'){
+      window.scrollTo({top:0,behavior:'smooth'})
+    }else {
+      const sectionId = path.replace('/','');
+       const element = document.getElementById(sectionId);
+       if(element){
+        element.scrollIntoView({behavior:'smooth'})
+       }
+    }
+  },[location]);
+  return null;
+ }
 
 
+ const AllSections  =() =>(
+  <>
+  <Hero />
+   <About />
+    <Testimonial />
+    <History />
+    
+  </>
+ )
 function App() {
 
   return (
   <> 
-<Navbar/>
-<Hero />
-<About />
-<Testimonial />
-<History/>
-<Footer/>
-<Signup />
-<Login />
+   <ScrollToSection />
+         <div className="bg-sky-300 min-h-screen text-black">
+<Navbar />
+   <Routes>
+    <Route path='/' element={<AllSections />}/>
+        <Route path='/about' element={<AllSections />}/>
+    <Route path='/testimonial' element={<AllSections />}/>
+    <Route path='/history' element={<AllSections />}/>
+    <Route path='/login' element={<Login />}/>
+    <Route path='/signup' element={<Signup />}/>
 
-{/* <Signup /> */}
-
+   </Routes>
+   <Footer />
+   </div>
   </>
   )
 }
