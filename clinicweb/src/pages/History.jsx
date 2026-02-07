99e23958ed7be9361d0a9f9b264323dc6287727f
shailendra pratap <singhlalprat@gmail.com>
2026-02-07 13:29:19 +0530

    Update History.jsx

--- clinicweb/src/pages/History.jsx
+++ clinicweb/src/pages/History.jsx
@@ -1,10 +1,12 @@
-import { MoveRight } from 'lucide-react'
+
 import React from 'react'
+
+import { MoveRight } from 'lucide-react'
 
 const History = () => {
   return (
-    <section className='min-h-screen bg-white mt-10'>
-      <div className='flex flex-col md:flex md:flex-row md:mx-20 md:my-20 gap-5'>
+    <section id='history' className='min-h-screen bg-white '>
+      <div className='flex flex-col md:flex md:flex-row md:mx-20 md:pt-9 gap-5'>
          <div className='flex flex-col  md:w-100 md:h-90 mx-5 my-5'>
          <h2 className='text-4xl font-bold '>The Future </h2>
         <h1 className='text-4xl font-bold text-blue-500'>of Quality Health</h1>
@@ -15,9 +17,9 @@ const History = () => {
 <img src="./o.jpg" className='h-90 w-120 rounded-2xl' alt="" />
          </div>
       </div>
-      <div className='bg-blue-500 md:h-70 ml-4 h-30 md:w-200 md:ml-15 rounded-2xl flex flex-col items-center  mt-30 '>
+      <div className='bg-blue-500 md:h-70 ml-4 h-30 md:w-200 md:ml-15 rounded-2xl flex flex-col items-center  mt-30  '>
        <h2 className='md:pt-15 md:text-3xl pt-5 font-bold text-gray-100 md:pl-0'>Subscribe to our newsletter</h2>
-        <div>
+        <div className=''>
          <form action="/search" className='md:mt-7  md:ml-10 w-50 md:w-170  md:h-13 h-10 rounded-b-4xl rounded-e-4xl rounded-4xl   bg-white md:mb-100 order-1 md:order-0 '
           >
                <div className='flex flex-row gap-5'>
