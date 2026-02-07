3378436d2369ac0b79e01be586a6182ef67eafd4
shailendra pratap <singhlalprat@gmail.com>
2026-02-07 17:08:52 +0530

    Update History.jsx

--- clinicweb/src/pages/History.jsx
+++ clinicweb/src/pages/History.jsx
@@ -1,5 +1,4 @@
 
-import React from 'react'
 
 import { MoveRight } from 'lucide-react'
 
@@ -17,25 +16,22 @@ const History = () => {
 <img src="./o.jpg" className='h-90 w-120 rounded-2xl' alt="" />
          </div>
       </div>
-      <div className='bg-blue-500 md:h-70 ml-4 h-30 md:w-200 md:ml-15 rounded-2xl flex flex-col items-center  mt-30  '>
-       <h2 className='md:pt-15 md:text-3xl pt-5 font-bold text-gray-100 md:pl-0'>Subscribe to our newsletter</h2>
+      <div className='bg-blue-500 md:h-70 ml-4 h-30 md:w-200 md:ml-15 rounded-2xl flex flex-col items-center  mt-20 mb-3 '>
+     <div className='mb-5'> <h2 className='md:pt-15 md:text-3xl pt-5 font-bold text-gray-100 md:pl-0'>Subscribe to our newsletter</h2></div>
+      
         <div className=''>
          <form action="/search" className='md:mt-7  md:ml-10 w-50 md:w-170  md:h-13 h-10 rounded-b-4xl rounded-e-4xl rounded-4xl   bg-white md:mb-100 order-1 md:order-0 '
           >
                <div className='flex flex-row gap-5'>
           <input  type="email"  name="email" id='email' placeholder='enter your email' className='md:w-90 md:ml-5 w-30 rounded-2xl border border-gray-100'/>
           
-           
+           <button className=''
+           >
         <MoveRight className='md:ml-48 mb-3 bg-blue-500 md:w-10 md:h-7 rounded-b-4xl rounded-e-4xl rounded-4xl text-white cursor-pointer mt-1 ml-4'/>
           
+          </button>
           </div>
-            {/* <div className='flex flex-row gap-5'>
-                <img src="./doct.svg" className='h-5  pl-4' alt="" />
-          <input  type="text"  name="" placeholder='Doctor name' className='w-20'/>
-          
-          <input type="text" name="" placeholder='area' className='w-20'/>
         
-          </div> */}
          </form>
           </div>
       </div>
