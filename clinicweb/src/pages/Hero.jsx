0c6c59413829654e883d4ccffc92e1cceda5affd
shailendra pratap <singhlalprat@gmail.com>
2026-02-07 17:08:48 +0530

    Update Hero.jsx

--- clinicweb/src/pages/Hero.jsx
+++ clinicweb/src/pages/Hero.jsx
@@ -11,26 +11,36 @@ const Hero = () => {
     </div>
       
         {/* <button className='mt-10 ml-38 h-10 w-20 text-xl font-semibold rounded-xl hover:bg-white hover:border hover:border-sky-400 bg-sky-500'>Explore</button> */}
-<div className='md:flex md:flex-row flex flex-col'>
+<div className='md:flex md:flex-row flex flex-col pl-9 md:pl-0'>
 
 
-          <form action="/search" className='md:mt-30  md:ml-10 w-75 md:w-100 h-10 rounded-2xl mt-6 ml-4 bg-white md:mb-100 order-1 md:order-0 '
+          <form action="/search" className='md:mt-30  md:ml-10 w-85 md:w-100 h-10 rounded-2xl mt-6 ml- bg-white md:mb-100 order-1 md:order-0 '
           >
-            <div className='flex flex-row gap-5'>
+            <div className='flex flex-row gap-5 px-2'>
                 <img src="./doct.svg" className='h-5  pl-4' alt="" />
           <input  type="text"  name="" placeholder='Doctor name' className='w-20'/>
-          
+          <img src="./map_10843254.png" className='h-5  pl-4' alt="" />
           <input type="text" name="" placeholder='area' className='w-20'/>
         
           </div>
-   <div className=' bg-blue-500 flex flex-row md:flex md:flex-row  md:mt-25  h-15 md:w-150 md:gap-15 mt-20 sm:mt-3 rounded-xl w-75'>
-             <h2 className='text-white text-5xl font-bold pl-3'>24/7 <span className='text-5xl font-light text-'>|</span></h2>
-                          <h2 className='text-white text-5xl font-bold pl-3'>24/7 <span className='text-5xl font-light text-'>|</span></h2>
-
+   <div className=' bg-blue-500 flex flex-row md:flex md:flex-row  md:mt-25  h-20 md:w-190 md:gap-15 mt-20 sm:mt-3 rounded-xl w-90'>
+            <div className='flex flex-col'>
+             <h2 className='text-white text-5xl font-bold '>24/7|</h2>
+             <h3 className='text-gray-200'>customer help</h3>
+             </div>
+             <div className='flex flex-col'>
+                          <h2 className='text-white text-5xl font-bold '>100+|</h2>
+                          <h2 className='text-gray-300'>Doctors</h2>
+       </div>
+         <div>
+                          <h2 className='text-white text-5xl font-bold '>1M+</h2>
+                          <h2 className='text-gray-100'>Active patients</h2>
+       </div>
          </div>
           </form>
-              <img src="./d.png" alt=""  className=' ml-6 md:ml-15 md:pb-4 h-80 w-100 '/>
-          
+          <div className=' h-90 px-5'>
+              <img src="./d.png" alt=""  className=' ml-6 md:ml-15 md:pb-4 h-85 w-140 '/>
+          </div>
                          
            
    </div>
