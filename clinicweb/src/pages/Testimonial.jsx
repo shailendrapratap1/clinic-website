9dc74e8cf213b1347a4c0b63bcc401c94a021239
shailendra pratap <singhlalprat@gmail.com>
2026-02-08 19:01:10 +0530

    Update Testimonial.jsx

--- clinicweb/src/pages/Testimonial.jsx
+++ clinicweb/src/pages/Testimonial.jsx
@@ -11,7 +11,7 @@ const Testimonial = () => {
       <p className='pt-5 text-xl font-semibold '>Lorem ipsum dolor, sit amet consectetur .</p>
       <p className=' text-xl font-semibold'>adipisicing elit. Vero, veritatis</p>
         </div>
-             <div className='flex flex-row md:flex md:flex-row   pt-15  md:ml-0'>
+             <div className='flex flex-row md:flex md:flex-row md:gap-5   pt-15  md:ml-0'>
            <img src="./pexels-danxavier-908602.jpg "className='hidden rounded-b-2xl  h-9 w-13 ' alt="" />
              <img src="./pexels-danxavier-908602.jpg "className='hidden rounded-b-2xl h-9 w-13' alt="" />
            <img src="./pexels-danxavier-908602.jpg "className='rounded-b-2xl h-9 w-13' alt="" />
@@ -22,17 +22,14 @@ const Testimonial = () => {
            <Card className='md:w-120 w-90 border border-gray-500 bg-white md:h-60 md:ml-19  md:-mt-50 rounded-xl flex flex-row mt-50 -ml-90  mb-3'>
                            <div className='flex flex-row gap-5'>
                            <img src="./pexels-danxavier-908602.jpg "className='rounded-b-2xl h-9 w-13 mt-5 ml-3 ' alt="" />
-                            <h2 className='mt-4 text-md font-bold '>john doe</h2>
+                            <h2 className='mt-4 text-md font-bold '>ronnie joy</h2>
                       
-                        <p className='mt-4 md:pl-40  '> ⭐ ⭐ ⭐ ⭐  </p>
+                        <p className='mt-4   '> ⭐ ⭐ ⭐ ⭐  </p>
                       </div>
-                        <div>
-                        <p className='text-gray-500 pt-10 font-medium px-2 py-2'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum dignissimos laudantium voluptates quasi, accusamus, aliquam temporibus facilis at repellendus quisquam eum nam distinctio omnis cum porro facere maiores? Minima voluptatum aspernatur vitae odit </p>
+                        <div className='flex'>
+                        <p className='text-gray-500  font-medium px-2 py-2'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsum dignissimos laudantium voluptates quasi, accusamus, aliquam temporibus facilis at repellendus quisquam eum nam dist </p>
                          </div>
            </Card>
-                    
-
-         
         </div>
      
      </div>
